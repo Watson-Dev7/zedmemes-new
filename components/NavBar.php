873df9dd0b7aebebc5e30a65f8c636284798ae02
joshua-inf <jsibanda407@gmail.com>
2025-07-18 05:37:13 +0200

    ssd

--- components/NavBar.php
+++ components/NavBar.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!-- NAVBAR -->
 <nav class="navbar">
   <div class="navbar-container">
@@ -11,20 +15,28 @@
 
     <!-- Menu Items -->
     <ul class="menu-items">
+
+            <?php if (isset($_SESSION['user_id'])): ?>
+                <!-- Only visible to logged-in users -->
       <li>
         <button class="menu-button" onclick="triggerThrobber()">Create Meme</button>
       </li>
       <li>
         <button class="menu-button" onclick="triggerThrobber()">Remove Meme</button>
       </li>
+            <?php else: ?>
+                <!-- Only visible to guests -->
       <li>
         <button class="menu-button" onclick="triggerThrobber()">Filter</button>
       </li>
+            <?php endif; ?>
+
+            <?php if (isset($_SESSION['user_id'])): ?>
+                <!-- Profile dropdown only for logged-in users -->
       <li class="profile-dropdown">
         <button id="profile-btn" class="icon-button" onclick="toggleProfile()">
           <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path
-              d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 
+                            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 
                  0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 
                  29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z" />
           </svg>
@@ -33,7 +45,7 @@
         <div id="profile-overlay" class="profile-overlay hidden">
           <div class="popup">
             <img src="zedmems.jpg" alt="Profile Picture" class="profile-pic">
-            <h3>@josephine</h3>
+                            <h3>@<?= htmlspecialchars($_SESSION['username'] ?? 'user') ?></h3>
             <p class="description">Meme queen of ZedMemes. Loves cats, coding, and comedy.</p>
             <div class="popup-actions">
               <button class="cta-button" onclick="logout()">Log Out</button>
@@ -42,6 +54,16 @@
           </div>
         </div>
       </li>
+            <?php else: ?>
+                <li>
+                    <a href="pages/login.php" class="menu-button login-btn">Login</a>
+                </li>
+                <li>
+                    <a href="pages/signup.php" class="menu-button signup-btn">Sign Up</a>
+                </li>
+            <?php endif; ?>
+
+
     </ul>
   </div>
 </nav>
